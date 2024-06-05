7cda571c9b3b741401d86ee2457557df627123ac
arvinzer <subhanaldiansyah3@gmail.com>
2024-06-05 11:42:27 +0800

    ,

--- database/migrations/2024_03_27_002755_create_penerimaans.php
+++ database/migrations/2024_03_27_002755_create_penerimaans.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('penerimaan', function (Blueprint $table) {
+        Schema::create('penerimaans', function (Blueprint $table) {
             $table->id();
             $table->date('tgl_transaksi');
             $table->integer('kd_akun');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('penerimaan');
+        Schema::dropIfExists('penerimaans');
     }
 };
